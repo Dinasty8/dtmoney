c1609e364add1b250c6ab00775c52f1531d96e92
Lucas Brito <lucas3213.lb@aluno.fapce.edu.br>
2022-01-05 19:38:58 -0300

    Calculando resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,9 +9,23 @@ import { Container } from "./style";
 
 export function Summary(){
 
-  const transactions =useContext(TransactionsContext);
-  console.log(transactions)
+  const {transactions} =useContext(TransactionsContext);
 
+  const summary = transactions.reduce((acc,transaction)=>{
+    if(transaction.type === 'deposit'){
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    }else{
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+    return acc;
+  },{
+    deposits:0,
+    withdraws:0,
+    total:0,
+  })
+//valores inicial 0
   return(
     <Container>
 
@@ -20,7 +34,12 @@ export function Summary(){
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR',{
+          style:"currency",
+          currency:"BRL"
+        }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -28,7 +47,12 @@ export function Summary(){
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          -  
+          {new Intl.NumberFormat('pt-BR',{
+          style:"currency",
+          currency:"BRL"
+        }).format(summary.withdraws)}</strong>
       </div>
 
       <div className='highlight-background'>
@@ -36,7 +60,12 @@ export function Summary(){
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR',{
+          style:"currency",
+          currency:"BRL"
+        }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
